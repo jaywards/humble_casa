3779c9b4577937b5a7cea790fc521d42d2dff07b
Jason <jason.wardwell@gmail.com>
2013-04-22 10:08:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HumbleCasa::Application.config.secret_token = 'f7cfe40d99075e7f25287e1b62400a487791606c5706cfe2f77e3749efdb0e71c94fbd26d975006257c8440fbc558ce89ea40ff81a84ed37c8c3b05e08828072'
