8a89aa898f5cdfa59794c93c8b7fed9a3ebd2346
Jason <jason.wardwell@gmail.com>
2013-07-22 12:05:58 -0700

    admin user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP ADMIN USER LOGIN'
+user = User.create! :first_name => 'Admin', :last_name => 'User', :email => 'admin@humblecasa.com', :password => '4INFO4life', 
+					:password_confirmation => '4INFO4life', :role => :admin, :primary_phone => 4444444444
+puts 'New user created: ' << user.first_name
